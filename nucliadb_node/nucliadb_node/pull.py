f575c7320dc5fd3c9ba8bfc4a7887e085cb8e01c
vangheem <vangheem@gmail.com>
2023-10-14 08:16:57 -0400

    skip resource that is failing

--- nucliadb_node/nucliadb_node/pull.py
+++ nucliadb_node/nucliadb_node/pull.py
@@ -267,6 +267,9 @@ class Worker:
     async def set_resource(self, pb: IndexMessage) -> OpStatus:
         self.brain = await self.storage.get_indexing(pb)
         self.brain.shard_id = self.brain.resource.shard_id = pb.shard
+        if self.brain.resource.uuid == "c6e7bd27a0d45587cc766910f00557ea":
+            print("Skipping message")
+            return OpStatus()
         logger.info(
             f"Added {self.brain.resource.uuid} at {self.brain.shard_id} otx:{pb.txid}"
         )
